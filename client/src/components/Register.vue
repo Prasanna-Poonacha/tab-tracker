ea5dc37a57548ba72589ad7476b831b8cbdd50ad
Poonacha <PP052697@cerner.net>
2017-12-07 21:46:36 +0530

    setup backend and register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email">
+      <br>
+    <input 
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password">
+      <br>
+    <button 
+      @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      // console.log('register method was clicked', this.email, this.password)
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed: ' + value)
+    }
+  },
+  mounted () {
+    console.log('mounted')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
